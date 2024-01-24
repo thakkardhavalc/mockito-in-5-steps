45c0d323fb4de29af1b559937c74853742c2c167
thakkardhavalc <thakkardhavalc@gmail.com>
2024-01-24 13:37:20 -0700

    initial commit

---
+++ src/main/java/com/in28minutes/mockito/mockitoin5steps/MockitoIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.mockito.mockitoin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoIn5StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/mockito/mockitoin5steps/MockitoIn5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.mockito.mockitoin5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoIn5StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
